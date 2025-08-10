ba3d652e24d7b52f530526f8ef6f04f55764933b
smallram <dominik.baranek1@gmail.com>
2025-08-11 01:56:24 +0200

    removed unused parts

--- Scripts/Gameplay/MemorySlot.gd
+++ Scripts/Gameplay/MemorySlot.gd
@@ -1,6 +1,3 @@
 extends Area2D
 @export var memory_id : String = ""
 @export var slot_idx : int = 0      # 0 = unassigned, give each slot a unique index
-	
-func get_circle_texture() -> Texture2D:
-	return MemoryPool.table.memory_to_circle_tex.get(memory_id, null)
