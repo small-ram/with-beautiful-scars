ae80f5ec390326ad2104a0380a01f72d7fb56f1d
unknown <dominik.baranek1@gmail.com>
2025-09-27 18:01:11 +0200

    vizual od Adely

--- Scripts/Gameplay/MemoryTable.gd
+++ Scripts/Gameplay/MemoryTable.gd
@@ -2,10 +2,10 @@ extends Resource
 class_name MemoryTable
 @export var slot_to_memory_id : Dictionary = {}
 @export var memory_to_circle_tex : Dictionary = {
-	"mem_crib":       preload("res://Assets/Circles/circle_crib.png"),
-	"mem_hehory":     preload("res://Assets/Circles/circle_horse.png"),
-	"mem_distance":   preload("res://Assets/Circles/circle_distance.png"),
-	"mem_repair":     preload("res://Assets/Circles/circle_repair.png"),
-	"mem_concept":    preload("res://Assets/Circles/circle_concept.png"),
-	"mem_closure":    preload("res://Assets/Circles/circle_closure.png")
+	"mem_crib":       preload("res://Assets/Blueberries/mem_crib.png"),
+	"mem_hehory":     preload("res://Assets/Blueberries/mem_hehory.png"),
+	"mem_distance":   preload("res://Assets/Blueberries/mem_distance.png"),
+	"mem_repair":     preload("res://Assets/Blueberries/mem_repair.png"),
+	"mem_concept":    preload("res://Assets/Blueberries/mem_concept.png"),
+	"mem_closure":    preload("res://Assets/Blueberries/mem_closure.png")
 }
